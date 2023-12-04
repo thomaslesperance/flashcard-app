f4d580e7efb16c06ac5f20c3dba67bbc08fad4ba
Thomas L'Esperance <144936700+thomaslesperance@users.noreply.github.com>
2023-12-04 15:05:02 -0600

    Further work on Deck view page.

--- src/Layout/BreadCrumbNav.js
+++ src/Layout/BreadCrumbNav.js
@@ -4,7 +4,7 @@ function BreadCrumbNav() {
   return (
     <div className="row mb-3">
       <div className="col">
-        <div className="card">
+        <div className="card" style={{ width: "90%" }}>
           <div className="card-body">
             <h5 className="card-title">Breadcrumb nav to go here...</h5>
           </div>
--- src/Layout/deck/Deck.js
+++ src/Layout/deck/Deck.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useHistory, useRouteMatch } from "react-router-dom";
-import { readDeck, deleteDeck } from "../../utils/api";
+import { readDeck, deleteDeck, deleteCard } from "../../utils/api";
 
 function Deck() {
   const [deck, setDeck] = useState({});
   const { deckId } = useParams();
   const history = useHistory();
   const { path } = useRouteMatch();
+  let cardList;
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -25,6 +26,7 @@ function Deck() {
     }
 
     loadDeck();
+    cardList = cardListMaker();
 
     return () => abortController.abort();
   }, [deckId]);
@@ -41,74 +43,121 @@ function Deck() {
     }
   }
 
-  if (deck.id) {
+  async function handleDeleteCard(cardId) {
+    const response = await deleteCard(cardId);
+    if (!Object.keys(response).length) {
+      const newCards = deck.cards.filter((card) => card.id !== cardId);
+      const newDeck = deck;
+      newDeck.cards = newCards;
+      setDeck(newDeck);
+    }
+  }
+
+  function cardListMaker() {
+    return deck.cards.map((card) => {
+      return (
+        <li class="list-group-item">
+          <div className="row">
+            <div className="col">{card.front}</div>
+            <div className="col">
+              <p>{card.back}</p>
+            </div>
+          </div>
+          <div className="row justify-content-end mr-3">
+            <button
+              type="button"
+              className="btn btn-secondary mr-4"
+              onClick={() => history.push(`${path}/cards/${card.id}/edit`)}
+            >
+              <i
+                className="bi bi-pencil"
+                style={{ marginRight: "10px", fontSize: "1.1rem" }}
+              ></i>
+              Edit
+            </button>
+            <button
+              type="button"
+              className="btn btn-danger"
+              onClick={handleDeleteCard(card.id)}
+            >
+              <i className="bi bi-trash3" style={{ fontSize: "1.2rem" }}></i>
+            </button>
+          </div>
+        </li>
+      );
+    });
+  }
+
+  if (deck.id && cardList.length) {
     return (
       <>
         <div className="row mb-4">
-          <div className="col">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">{deck.name}</h5>
-                <p className="card-text">{deck.description}</p>
-                <button
-                  type="button"
-                  className="btn btn-secondary mr-2"
-                  onClick={() => history.push(`${path}/edit`)}
-                >
-                  <i
-                    className="bi bi-pencil"
-                    style={{ marginRight: "10px", fontSize: "1.1rem" }}
-                  ></i>
-                  Edit
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-primary mr-2"
-                  onClick={() => history.push(`${path}/study`)}
-                >
-                  <i
-                    className="bi bi bi-book"
-                    style={{ marginRight: "10px", fontSize: "1.1rem" }}
-                  ></i>
-                  Study
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-primary mr-5"
-                  onClick={() => history.push(`${path}/cards/new`)}
-                >
-                  <i
-                    className="bi bi bi-file-plus"
-                    style={{ marginRight: "10px", fontSize: "1.1rem" }}
-                  ></i>
-                  Add Cards
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-danger ml-md-5"
-                  onClick={handleDeleteDeck}
-                >
-                  <i
-                    className="bi bi-trash3"
-                    style={{ fontSize: "1.1rem" }}
-                  ></i>
-                </button>
+          <div className="card " style={{ width: "90%" }}>
+            <div className="card-body">
+              <h5 className="card-title">{deck.name}</h5>
+              <p className="card-text">{deck.description}</p>
+              <div className="row justify-content-start">
+                <div className="col-2 mr-1">
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    onClick={() => history.push(`${path}/edit`)}
+                  >
+                    <i
+                      className="bi bi-pencil"
+                      style={{ marginRight: "10px", fontSize: "1.1rem" }}
+                    ></i>
+                    Edit
+                  </button>
+                </div>
+                <div className="col-2 mr-1">
+                  <button
+                    type="button"
+                    className="btn btn-primary "
+                    onClick={() => history.push(`${path}/study`)}
+                  >
+                    <i
+                      className="bi bi bi-book"
+                      style={{ marginRight: "10px", fontSize: "1.1rem" }}
+                    ></i>
+                    Study
+                  </button>
+                </div>
+                <div className="col-2 mr-1">
+                  <button
+                    type="button"
+                    className="btn btn-primary "
+                    onClick={() => history.push(`${path}/cards/new`)}
+                  >
+                    <i
+                      className="bi bi bi-file-plus"
+                      style={{ marginRight: "10px", fontSize: "1.1rem" }}
+                    ></i>
+                    Add Cards
+                  </button>
+                </div>
+                <div className="col-2 offset-3">
+                  <button
+                    type="button"
+                    className="btn btn-danger"
+                    onClick={handleDeleteDeck}
+                  >
+                    <i
+                      className="bi bi-trash3"
+                      style={{ fontSize: "1.1rem" }}
+                    ></i>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div className="row">
-          <div className="col">
-            <div className="card">
-              <div className="card-header border-0">
-                <h5>Cards</h5>
-              </div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">A deck card</li>
-                <li class="list-group-item">A second deck card</li>
-                <li class="list-group-item">A third deck card</li>
-              </ul>
+          <div className="card" style={{ width: "90%" }}>
+            <div className="card-header border-0">
+              <h5>Cards</h5>
             </div>
+            <ul class="list-group list-group-flush">{cardList}</ul>
           </div>
         </div>
       </>
@@ -119,21 +168,3 @@ function Deck() {
 }
 
 export default Deck;
-
-// {
-//   "decks": [
-//     {
-//       "id": 1,
-//       "name": "...",
-//       "description": "..."
-//     }
-//   ],
-//   "cards": [
-//     {
-//       "id": 1,
-//       "front": "...",
-//       "back": "...",
-//       "deckId": 1
-//     }
-//   ]
-// }
